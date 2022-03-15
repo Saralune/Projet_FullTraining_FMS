021b2e9706750c2f74d0204a0cf44f5280cb05a4
Sarah Lefort <sarahlef59@hotmail.fr>
2022-03-15 16:00:04 +0100

    add menu & interaction with user

--- FullTraining/src/Main.java
+++ FullTraining/src/Main.java
@@ -1,30 +1,50 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
 
 /**
  * Application permettant d'acheter des formations en ligne
  * */
 public class Main {
-	/****************NVLLE BRANCHE POUR NOUVELLE FOCNTIONNALITé******************/
-	//Menu : 
-		//- liste des formations
-		//- Afficher le contenu du panier
-		//- Ajouter une formation à son panier (possible d'ajouter plusieurs fois la même)
-		//- Supprimer une formation du panier
-	
-	//Passer commande --> "Autre écran"
-		//- Récap avec le contenu du panier + montant total
-		//- Quand valid� => message de confirmation + panier vidé + appli qui revient sur le menu principal
-	
-	//Ajouter des formations prochainement disponibles
-	
-	//Fonctionnalité de mon choix
 
 	public static void main(String[] args) {
+		/*INIT*/
+		Scanner scanner = new Scanner(System.in);
+		
+		HashMap<Integer, ArrayList<String>> basket = new HashMap<Integer, ArrayList<String>>();
+		/*END INIT*/
+		
 		System.out.println("Bonjour et bienvenue dans mon application FullTraining.");
 		System.out.println("Nous allons vous proposer une liste de formation actuellemnt disponibles : ");
+		initAllTrainings();
+		printTrainings();
 		
-		displayAllTraining(initAllTraining());
+		printMenu();
+		int answer = scanner.nextInt();
+		
+		//A VOIR SI AUTRE R2PONSE ????
+		//while(scanner.hasNextInt()) {
+			switch(answer) {
+				case 1:
+					printTrainings();
+					break;
+					
+				case 2:
+					printTrainingsToChoose();
+					addTrainingToBasket(basket, answer);
+					break;
+					
+				case 3:
+					printBasket();
+					break;
+					
+			}
+		//}
+		
+		
+		scanner.close();
+		
 	}
 	
 	/**Function that initializes all training available (name, duration, description, price)
@@ -33,7 +53,7 @@ public class Main {
 	 * @return void
 	 * 
 	 * */
-	public static ArrayList<String[]> initAllTraining() {
+	public static ArrayList<String[]> initAllTrainings() {
 		ArrayList<String[]> trainingList = new ArrayList<>();
 				
 		String[] training1 = {"1", "Java", "10", "Les bases", "1000"};
@@ -51,18 +71,20 @@ public class Main {
 		return trainingList;
 	}
 	
-	/**Function that print all training initialized
+	/**Function that print all training available (name, duration, description, price)
 	 * 
 	 * @author Stagiaires10P
 	 * @return void
 	 * 
 	 * */
-	public static void displayAllTraining(ArrayList<String[]> trainingList) {
-		String format  = "%1$-8s | %2$-8s | %3$-40s | %4$-8s\n";
+	public static void printTrainings() {
+		ArrayList<String[]> trainingList = initAllTrainings();
+		
+		String format  = "%1$-8s | %2$-8s | %3$-40s | %4$-6s |\n";
 		
 		System.out.println(String.join("", Collections.nCopies(73, "-")));
 		System.out.format(format, "COURS", "NB/JOURS", "DESCRIPTION", "PRIX");
-		System.out.format(format, "--------", "--------", String.join("", Collections.nCopies(40, "-")), "--------");
+		System.out.format(format, "--------", "--------", String.join("", Collections.nCopies(40, "-")), "------");
 		
 		for (int i = 0; i < trainingList.size(); i++) {
 			System.out.format(format, 
@@ -73,7 +95,65 @@ public class Main {
 		}
 		
 		System.out.println(String.join("", Collections.nCopies(73, "-")));
+		System.out.println();
+		
+	}
+	
+	/**Function that print all training when user want to add one to his basket.
+	 * 
+	 * @author Stagiaires10P
+	 * @return void
+	 * 
+	 * */
+	public static void printTrainingsToChoose() {
+		ArrayList<String[]> trainingList = initAllTrainings();
+		String format  = "%1$-6s | %2$-8s | %3$-8s | %4$-40s | %5$-6s |\n";
+		
+		System.out.println(String.join("", Collections.nCopies(82, "-")));
+		System.out.format(format,"CHOIX", "COURS", "NB/JOURS", "DESCRIPTION", "PRIX");
+		System.out.format(format, "------",  "--------", "--------", String.join("", Collections.nCopies(40, "-")), "------");
+		
+		for (int i = 0; i < trainingList.size(); i++) {
+			System.out.format(format, 
+					trainingList.get(i)[0],
+					trainingList.get(i)[1], 
+					trainingList.get(i)[2], 
+					trainingList.get(i)[3], 
+					trainingList.get(i)[4]);
 		}
+		
+		System.out.println(String.join("", Collections.nCopies(82, "-")));
+	}
+	
+	/** Print menu. It will display all choices for user.
+	 * 
+	 * */
+	public static void printMenu() {
+		System.out.println("Que souhaitez-vous faire ?");
+		System.out.println("1. Afficher la liste des formations.");
+		System.out.println("2. Ajouter une formation à mon panier.");
+		System.out.println("3. Afficher le contenu de mon panier.");
+		
+		//System.out.println("2. ");
+		//System.out.println("2. ");
+		//System.out.println("2. ");
+		
+		System.out.println("X. Quitter l'application.");
+
+	}
+	
+	public static void addTrainingToBasket(HashMap<Integer, ArrayList<String>> basket, int id) {
+		//ajouter à la hashmap avec integer = id
+		//pour savoir si plusieurs ajout : taille de la arraylist
+		
+		System.out.println("Vous avez ajouté la leçon : " + "truc");
+	}
+	
+	public static void printBasket() {
+		
+		//sous forme de tableau
+		//ligne : cours - durée [1] jours - qté - prix unitaire - prix total
+	}
 	
 	/** Add a course to my list of training
 	 * 
